6d56ae3eb32d1ceb0aefa5a3cca25e77925bd360
Rathanak Sreang <sreang.rathanak@framgia.com>
2015-05-08 15:43:56 +0700

    User Profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,11 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+  
+  def gravatar_for user, options = {}
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url += "?s=#{options[:size]}" if options[:size]
+    image_tag gravatar_url, alt: user.username, class: "gravatar"
+  end
 end
